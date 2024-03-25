698a113315c59b00e730f6975c543f42dc0db8dd
Fernando Nicolás <fersanti2896@gmail.com>
2024-03-25 13:26:24 -0600

    Mostrando contenido en componente de Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 
-function Header() {
+function Header({ cart }) {
+    //* State Derivado
+    const isEmpty = () => cart.length === 0
 
     return (
         <>
@@ -23,45 +25,54 @@ function Header() {
                                     alt="imagen carrito"
                                 />
                                 <div id="carrito" className="bg-white p-3">
-                                    <p className="text-center">El carrito esta vacio</p>
-                                    <table className="w-100 table">
-                                        <thead>
-                                            <tr>
-                                                <th>Imagen</th>
-                                                <th>Nombre</th>
-                                                <th>Precio</th>
-                                                <th>Cantidad</th>
-                                                <th></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>
-                                                    <img
-                                                        className="img-fluid"
-                                                        src="./public/img/guitarra_02.jpg"
-                                                        alt="imagen guitarra"
-                                                    />
-                                                </td>
-                                                <td>SRV</td>
-                                                <td className="fw-bold">$299</td>
-                                                <td className="flex align-items-start gap-4">
-                                                    <button type="button" className="btn btn-dark">
-                                                        -
-                                                    </button>
-                                                    1
-                                                    <button type="button" className="btn btn-dark">
-                                                        +
-                                                    </button>
-                                                </td>
-                                                <td>
-                                                    <button className="btn btn-danger" type="button">
-                                                        X
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                                    {isEmpty()
+                                        ? (<p className="text-center">El carrito esta vacio</p>)
+                                        : (
+                                            <table className="w-100 table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Imagen</th>
+                                                        <th>Nombre</th>
+                                                        <th>Precio</th>
+                                                        <th>Cantidad</th>
+                                                        <th></th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {
+                                                        cart.map(({ id, name, price, quantity, image }) => (
+                                                            <tr key={id}>
+                                                                <td>
+                                                                    <img
+                                                                        className="img-fluid"
+                                                                        src={`/img/${image}.jpg`}
+                                                                        alt="imagen guitarra"
+                                                                    />
+                                                                </td>
+                                                                <td>{name}</td>
+                                                                <td className="fw-bold">{price}</td>
+                                                                <td className="flex align-items-start gap-4">
+                                                                    <button type="button" className="btn btn-dark">
+                                                                        -
+                                                                    </button>
+                                                                    {quantity}
+                                                                    <button type="button" className="btn btn-dark">
+                                                                        +
+                                                                    </button>
+                                                                </td>
+                                                                <td>
+                                                                    <button className="btn btn-danger" type="button">
+                                                                        X
+                                                                    </button>
+                                                                </td>
+                                                            </tr>
+                                                        ))
+                                                    }
+
+                                                </tbody>
+                                            </table>
+                                        )
+                                    }
 
                                     <p className="text-end">
                                         Total pagar: <span className="fw-bold">$899</span>
